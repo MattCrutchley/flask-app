1e42c11f6b0eea158ed4084c6b3e3e1c4efa748a
MattCrutchley <mcrutchley8@gmail.com>
2020-02-21 12:54:16 +0000

    bolg posts

--- application/routes.py
+++ application/routes.py
@@ -2,9 +2,32 @@
 from flask import render_template
  # import the app object from the ./application/__init__.py
 from application import app
-
+blogData = [
+    {  
+        "name": {"first":"John", "last":"Doe"},
+        "title":"First Post",
+        "content":"This is some blog data for Flask lectures"
+    },
+    {   
+        "name": {"first":"Jane", "last":"Doe"},
+        "title":"Second Post",
+        "content":"This is even more blog data for Flask lectures"
+    }
+]
  # define routes for / & /home, this function will be called when these are accessed
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('home.html', title='home',posts = blogData)
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title ='register')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title ='about')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title ='login')
