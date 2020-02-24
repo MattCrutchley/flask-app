30c2449bc68dea18d5d4318428fcd30a726141a2
MattCrutchley <56794925+MattCrutchley@users.noreply.github.com>
2020-02-24 11:32:23 +0000

    Update __init__.py

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,9 @@
- # import Flask class from the flask module
+import os
+# import Flask class from the flask module
 from flask import Flask
  # create a new instance of Flask and store it in app 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+
  # import the ./application/routes.py file
 from application import routes
