f99caf720dd947717237d19b4452446e3ad659ed
Ubuntu <matt@flask.nofo3zbomk4ebd0i5uaok2khcc.ax.internal.cloudapp.net>
2020-02-24 14:05:02 +0000

    connected to db

--- application/__init__.py
+++ application/__init__.py
@@ -2,8 +2,9 @@ import os
 # import Flask class from the flask module
 from flask import Flask
  # create a new instance of Flask and store it in app 
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
-
+db = SQLAlchemy(app)
  # import the ./application/routes.py file
 from application import routes
