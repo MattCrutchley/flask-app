b21c161805ebab88cb4c3e8aee37dea06fe64c8f
Ubuntu <matt@flask-bash.hmrweiuza1uetedbvongfs31qe.ax.internal.cloudapp.net>
2020-03-03 11:58:21 +0000

    updated bash script

--- deployflask/environment.sh
+++ deployflask/environment.sh
@@ -1,22 +1,21 @@
 #! /bin/bash
-sudo apt-get update 
-sudo apt-get install mysql-server
-sudo apt-get install python3
-sudo apt-get install python3-venv
-sudo apt-get install pip3 -y
+sudo apt-get update -y
+sudo apt-get install mysql-server -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-venv -y
+sudo apt-get install python3-pip -y
+cd 
 echo "enter name for database"
 read dbname
 echo "enter username for new mysql user"
 read sqluser
 echo "enter password for new mysql user"
 read sqlpass
-cd 
+cd ~/flask-app/
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 export DATABASE_URI="mysql+pymysql://${sqluser}:${sqlpass}@127.0.0.1/${dbname}"
 echo "enter SECRET_KEY"
 read SKEY
-export SECRET_KEY="${SKEY}
-
-
+export SECRET_KEY="${SKEY}"
--- deployflask/runflask.sh
+++ deployflask/runflask.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-cd
+cd ~/flask-app/
+source venv/bin/activate
 python3 create.py
 python3 app.py
